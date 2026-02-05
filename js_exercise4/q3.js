cbe428a8f31398b355626ccb0a86c4546877915c
HrshRaj30062004 <harshraj.manu@gmail.com>
2026-02-05 17:17:02 +0530

    Added exercise 4

---
+++ js_exercise4/q3.js
@@ -0,0 +1,10 @@
+let user={ name:"Piyush", age:24 }
+
+const {name}= user;
+
+console.log(name);
+
+const { name: myName } = { name: 'Lydia' };
+
+console.log(name); 
+
---
+++ js_exercise4/q4.js
@@ -0,0 +1,4 @@
+console.log({a:1} == {a:1});
+
+console.log({a:1} === {a:1});
+
